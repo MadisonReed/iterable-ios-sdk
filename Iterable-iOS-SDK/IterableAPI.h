2e03b7b08adc2fe71b3ee5c372029ec50eec0add
Justin Zealand <justin@madison-reed.com>
2016-06-23 11:12:09 -0700

    Modified to allow for create and send request methods to be public

--- Iterable-iOS-SDK/IterableAPI.h
+++ Iterable-iOS-SDK/IterableAPI.h
@@ -317,6 +317,10 @@ typedef NS_ENUM(NSInteger, PushServicePlatform) {
  */
 - (void)track:(NSString *)eventName dataFields:(nullable NSDictionary *)dataFields onSuccess:(OnSuccessHandler)onSuccess onFailure:(OnFailureHandler)onFailure;
 
+#warning JZ Locally modified
+- (NSURLRequest *)createRequestForAction:(NSString *)action withArgs:(NSDictionary *)args;
+- (void)sendRequest:(NSURLRequest *)request onSuccess:(void (^)(NSDictionary *))onSuccess onFailure:(void (^)(NSString *, NSData *))onFailure;
+
 @end
 
 NS_ASSUME_NONNULL_END
